c6b0d37af247f9ce0e46ca20976821a277e8294b
疾风 <mzhua78@hotmail.com>
2014-07-30 23:27:11 +0800

    配置环境

---
+++ src/com/jinch/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.jinch.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/**
+ * Created by 敏华 on 2014/7/30.
+ */
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(generator="uuid")
+    @GenericGenerator(name="uuid",strategy="assigned")
+    @Column(name="data_id",length=32)
+    protected String dataId;
+}
